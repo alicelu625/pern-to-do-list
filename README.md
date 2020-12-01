3cfa054ec3e4c0a5ae7e11420d473b62f80e9996
Zifei Lu <zlu27@ucsc.edu>
2020-12-01 13:38:57 -0800

    completed application

---
+++ README.md
@@ -0,0 +1,19 @@
+## To Do List (PERN)
+
+***This application is still being worked on for deployment.***
+
+What I used:
+* PERN stack (PostgreSQL, Express.js, React.js, Node.js)
+* JavaScript
+* HTML 5
+* Bootstrap
+* Webpack
+* CORS
+* pg (node-postgres)
+* RESTful APIs
+
+The To Do List application allows a user to add, edit, and delete to do list items by sending GET, PUT, POST, and DELETE API requests to the server.
+
+The application was modified from the [PERN Stack Course](https://www.youtube.com/watch?v=ldYcgPKEZC8&t=13s).
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+const pool = require('./db');
+
+const app = express();
+
+/*MIDDLEWARE*/
+app.use(cors()); //allow different domains on server to interact with each other
+app.use(express.json()); //enable get json data from request body
+
+/*LISTENER*/
+//env variable for port if set, otherwise, use 5000
+const port = process.env.PORT || 5000;
+//listen on port
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
+/*ROUTES*/
+
+//create a todo
+app.post('/todos', async(req, res) => {
+    try {
+        const {description} = req.body;
+        //query to insert todo
+        const newTodo = await pool.query(
+            //RETURNING * - returns data
+            "INSERT INTO todo (description) VALUES($1) RETURNING * ",
+            [description] //into $1
+        );
+        //return inserted todo
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get all todos
+app.get('/todos', async (req, res) => {
+    try {
+        //query to get all todos
+        const allTodos = await pool.query("SELECT * FROM todo");
+        //return all todos from query result
+        res.json(allTodos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get a todo
+app.get('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        //query to get todo by with specified id
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+        //return todo from query result
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//update a todo
+app.put('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        //query to update to do
+        const updateTodo = await pool.query(
+            "UPDATE todo SET description = $1 WHERE todo_id = $2 RETURNING *",
+            [description, id]
+        );
+        res.json("Todo was updated");
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//delete a todo
+app.delete('/todos/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const deletedTodo = await pool.query(
+            "DELETE FROM todo WHERE todo_id = $1 RETURNING *",
+            [id]  
+        );
+        res.json("To do was deleted");
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
